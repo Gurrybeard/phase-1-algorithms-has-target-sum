4e0680297db255d73538e786610845da6420dfbb
Benjamin <bboksner@gmail.com>
2022-03-15 10:32:31 -0400

    commit tree 28c1c966de60087601278b2c22ca0b0f7b94d19a

--- index.js
+++ index.js
@@ -1,13 +1,66 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
 
+  for(let i =0; i<array.length; i++){
+    let comp = target - array[i];
+    for(let j=1; j<array.length; j++){
+      if(array[j] === comp){
+        
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /*
+  let compliment;
+  array.forEach(element => {
+     compliment = target- element;
+    let foundNum = array.find((e)=>{
+        return e === compliment;
+     })
+      console.log(`found num: ${foundNum}`)
+     if(foundNum +element === target){
+       return true;
+     }
+     else if(foundNum === undefined){
+       return false;
+     }
+     else{
+       return undefined;
+     }
+  });
+  */
+
+}
+  //filterd array for compliment number
+   /*
+ let foundNum = array.find((e)=>{
+  return e+ compliment=== target;
+ })
+
+
+
+
+if(addedNum ===target){
+  return true;e+ compliment === target
+}
+else{
+  return false;
+}
+}
+*/
 /* 
   Write the Big O time complexity of your function here
 */
 
 /* 
   Add your pseudocode here
+  for each {
+    in a for each{
+      that loops through the array for each item of the array
+    }
+  }
 */
 
 /*
